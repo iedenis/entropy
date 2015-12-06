760be25e6b611ca4d05a0970942c918d57c384c3
Denis <denis.ievlev@gmail.com>
2015-12-06 05:04:31 +0200

    fixing bugs in calculating entropy still doesn't work properly

---
+++ src/MyChar.cpp
@@ -0,0 +1,22 @@
+/*
+
+ * MyChar.cpp
+ *
+ *  Created on: Dec 5, 2015
+ *      Author: fox
+ */
+#include "MyChar.hpp"
+#include <iostream>
+int MyChar::total=0;
+double MyChar::entropy=0.0;
+std::ostream& operator<<(std::ostream &os, const MyChar &ch){
+	std::cout<<"My Char "<<std::endl;
+		return os;
+}
+
+
+
+
+
+
+
---
+++ src/MyChar.hpp
@@ -0,0 +1,24 @@
+/*
+ * MyChar.hpp
+ *
+ *  Created on: Dec 1, 2015
+ *      Author: fox
+ */
+
+#ifndef MYCHAR_HPP_
+#define MYCHAR_HPP_
+#include <string>
+#include <iostream>
+class MyChar {
+private:
+
+public:
+	static double entropy;
+	static int total;
+//MyChar& operator++(int);
+
+};
+
+std::ostream& operator<<(std::ostream &os, const MyChar &ch);
+
+#endif /* MYCHAR_HPP_ */
